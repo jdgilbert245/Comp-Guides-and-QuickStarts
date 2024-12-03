23a2d6300f2c9ed3c324ea678713ea71981c00f3
jdgilbert245 <james.d.gilbert@ucl.ac.uk>
2024-12-03 10:44:54 +0000

    make envs dir and add convert nb template

---
+++ environments/template-convertPyNb.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+### convert python notebook to report
+## note we don't want to show code- just markdown text and what is produced by code cells
+DIR=/path/to/notebook/folder
+FILE=filename.ipynb
+
+### nbconvert available in python environment: py_convert_nb
+## env yaml available on github
+conda activate py_convert_nb
+
+### convert notebook to html 
+## no input removes content of code cells
+jupyter notebook --no-input --to html ${DIR}/${FILE} # can also be --to pdf
